f04d7c60457e639af16fa8be4b689c80ea8375ef
vermatijil <106467874+vermatijil@users.noreply.github.com>
2023-09-15 23:30:37 +0530

    Add files via upload

---
+++ UniquePath.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int UniquePath(int n, int m)
+{
+    vector<vector<int>> dp(m, vector<int>(n, 1));
+    for (int i = 1; i < m; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        }
+    }
+    return dp[m - 1][n - 1];
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int m, n;
+        cin >> m >> n;
+        cout << UniquePath(m, n);
+        cout << endl;
+    }
+    return 0;
+}
